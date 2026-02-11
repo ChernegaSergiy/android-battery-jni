a9d822f10b01f7fbebd8992bab517ed8a2f7d573
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-11 06:54:12 +0000

    fix: Fix package name mismatch between build.gradle and Java sources

--- app/src/main/java/com/chernegasergiy/battery/fragment/AboutFragment.java
+++ app/src/main/java/com/chernegasergiy/battery/fragment/AboutFragment.java
@@ -8,13 +8,14 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import com.chernegasergiy.battery.R;
 
 public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(com.chernegasergiy.battery.R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
 
-        TextView versionText = view.findViewById(com.chernegasergiy.battery.R.id.about_version);
+        TextView versionText = view.findViewById(R.id.about_version);
         try {
             PackageInfo pInfo = requireActivity().getPackageManager().getPackageInfo(
                     requireActivity().getPackageName(), 0);
--- app/src/main/java/com/chernegasergiy/battery/fragment/StatusFragment.java
+++ app/src/main/java/com/chernegasergiy/battery/fragment/StatusFragment.java
@@ -6,14 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+import com.chernegasergiy.battery.R;
 
 public class StatusFragment extends Fragment {
     private TextView statusText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(com.chernegasergiy.battery.R.layout.fragment_status, container, false);
-        statusText = view.findViewById(com.chernegasergiy.battery.R.id.status_text);
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+        statusText = view.findViewById(R.id.status_text);
         statusText.setText("Listening for broadcasts.\nBattery data will be available\nwhen PHP extension connects.");
         return view;
     }
